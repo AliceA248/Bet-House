a7b48839270947bc041bdef14e870a83242d6441
AliceA248 <98560120+AliceA248@users.noreply.github.com>
2023-10-04 14:01:13 -0300

    partipant.repository-correções no func update

--- src/repositories/game.repository.ts
+++ src/repositories/game.repository.ts
@@ -4,50 +4,52 @@ import { GameInput, GameUpdate } from '../protocols';
 import { betRepository } from './bet.repository';
 import { participantRepository } from './participant.repository';
 
-function create(data:GameInput){
-	return prisma.game.create({data});
+function create(data: GameInput) {
+  return prisma.game.create({ data });
 }
 
-async function update(id:number, data:GameUpdate){
-	return await prisma.$transaction(async () => {
-		const [game, bets] = await Promise.all([
-			prisma.game.update({where:{id}, data:{...data, isFinished:true}}),
-			prisma.bet.findMany({where:{gameId:id}}),
-		]);
+async function update(id: number, data: GameUpdate) {
+  return await prisma.$transaction(async () => {
+    const [game, bets] = await Promise.all([
+      prisma.game.update({ where: { id }, data: { ...data, isFinished: true } }),
+      prisma.bet.findMany({ where: { gameId: id } }),
+    ]);
 
-		const { allBets, allWinnerBets } = betsResultsCalculator(bets, data);
+    const { allBets, allWinnerBets } = betsResultsCalculator(bets, data);
 
-		for(const bet of bets){
-			const won = (data.awayTeamScore === bet.awayTeamScore) && (data.homeTeamScore === bet.homeTeamScore);
-			const amountWon = won ? amountWonCalculator(bet.amountBet, allWinnerBets, allBets) : 0;
-			const status = won ? 'WON' : 'LOST';
+    const updates = bets.map(async (bet) => {
+      const won = data.awayTeamScore === bet.awayTeamScore && data.homeTeamScore === bet.homeTeamScore;
+      const amountWon = won ? amountWonCalculator(bet.amountBet, allWinnerBets, allBets) : 0;
+      const status = won ? 'WON' : 'LOST';
 
-			await Promise.all([
-				betRepository.update(bet.id, {status, amountWon}, prisma),
-				participantRepository.update(bet.participantId, amountWon, prisma)
-			]);
-		}
+      await Promise.all([
+        betRepository.update(bet.id, { status, amountWon }, prisma),
+        participantRepository.update(bet.participantId, amountWon, prisma),
+      ]);
+    });
 
-		return game;
-	});
+    await Promise.all(updates);
+
+    return game;
+  });
 }
 
-function findMany(){
-	return prisma.game.findMany();
+function findMany() {
+  return prisma.game.findMany();
 }
 
-function findOne(id:number){
-	return prisma.game.findUnique({
-		where:{id},
-		include:{
-			Bet:true
-		}
-	});
+function findOne(id: number) {
+  return prisma.game.findUnique({
+    where: { id },
+    include: {
+      Bet: true,
+    },
+  });
 }
 
 export const gameRepository = {
-	create,
-	update,
-	findMany,
-	findOne
+  create,
+  update,
+  findMany,
+  findOne,
 };
--- src/repositories/participant.repository.ts
+++ src/repositories/participant.repository.ts
@@ -2,32 +2,32 @@ import { PrismaClient } from '@prisma/client';
 import { prisma } from '../config';
 import { ParticipantInput } from '../protocols';
 
-function create(data:ParticipantInput){
-	return prisma.participant.create({data});
+function create(data: ParticipantInput) {
+  return prisma.participant.create({ data });
 }
 
-function findMany(){
-	return prisma.participant.findMany();
+function findMany() {
+  return prisma.participant.findMany();
 }
 
-function findOne(id:number){
-	return prisma.participant.findUnique({where:{id}});
+function findOne(id: number) {
+  return prisma.participant.findUnique({ where: { id } });
 }
 
-function update(id:number, increment:number, prispaTransaction:PrismaClient){
-	return prispaTransaction.participant.update({
-		where: { id },
-		data: {
-			balance : {
-				increment
-			}
-		}
-	});
+function update(id: number, increment: number, prismaTransaction: PrismaClient) {
+  return prismaTransaction.participant.update({
+    where: { id },
+    data: {
+      balance: {
+        increment,
+      },
+    },
+  });
 }
 
 export const participantRepository = {
-	create,
-	findMany,
-	findOne,
-	update
+  create,
+  findMany,
+  findOne,
+  update,
 };
